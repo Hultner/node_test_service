dd708944a32a89dceafe3176dbcab1e95985ffe4
Alexander Hultnér <ahultner@gmail.com>
2017-02-16 16:43:44 +0100

    Added async test injecting requests into the server

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,12 +1,29 @@
-//import server from '../src/server';
+import server from '../src/server';
 import HelloWorld from '../src/api/hello-world';
 // import http from 'http';
 
 //console.log(server);
+describe('HTTP Server Injection tests', () => {
 
-test('hello-world test', () => {
-    var hello = new HelloWorld();
-    expect(hello.get()).toBe('Hello World!');
+    beforeAll((done) => server.once('onPostStart', done));
+    
+    afterAll(() => server.stop()); 
+    
+
+    test('Injection test', async () => {
+        var data = await server.inject('/');
+        expect(data.statusCode).toBe(200);
+    });
+
+});
+
+describe('API Class tests', () => {
+    
+    test('hello-world test', () => {
+        var hello = new HelloWorld();
+        expect(hello.get()).toBe('Hello World!');
+    });
+
 });
 
 // test('hello-world test', async () => {
