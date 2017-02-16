1654e6089271b85f59217616757852dcaf80a237
Alexander Hultnér <ahultner@gmail.com>
2017-02-16 17:49:45 +0100

    Further documentation of tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,12 +8,14 @@ import HelloWorld from '../src/api/hello-world';
 // These test against the server api and therefor have to wait for server setup
 describe('HTTP Server Injection tests', () => {
 
+    // Will error it tries to use the server before it started
     beforeAll((done) => server.once('onPostStart', done));
     afterAll(() => server.stop()); 
     
 
     test('Root path', async () => {
         var data = await server.inject('/');
+        // Payload is returned as a string, therefor needs parsing.
         var payload = JSON.parse(data.payload);
         expect(data.statusCode).toBe(200);
         expect( payload.root ).toBe(true);
