b02530b9c01900a859054201b12a3c0a89b123b8
Alexander Hultnér <ahultner@gmail.com>
2017-02-16 16:43:18 +0100

    Added event channel and export making post start hooks possible

--- src/server.js
+++ src/server.js
@@ -24,12 +24,20 @@ server.route({
     }
 });
 
+// Register event channel
+server.event('onPostStart');
+
 server.start( err => {
     if (err) {
         // TODO: Add more robust and persistent error handling
         console.error( 'Error was handled!');
         console.error(err);
     }
+
     console.log("Server started at " + server.info.uri );
+
+    // Emit event announcing that the server have started
+    server.emit('onPostStart', 'hello');
 });
 
+export default server;
