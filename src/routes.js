25cc6bc389475271da1bdd13db8f4bd531d92a20
Alexander Hultnér <ahultner@gmail.com>
2017-02-16 16:42:07 +0100

    Added skeleton for future api abstraction

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+/* Routes
+ * 
+ * Some kind of future api abstraction layer which will allow to us to define 
+ * routes for hapi from api classes.
+ * Still working out exactly how we want to implement this, prefably in a 
+ * portable way.
+ */ 
+
+const routes = generateRoutes();
+
+generateRoutes(){
+    //If function get post put delete@path
+}
+
+export default routes;
