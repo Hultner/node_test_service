8abb7f0c9445f5de42dcc3850e21ebcdf824ee14
Alexander Hultnér <ahultner@gmail.com>
2017-03-01 17:12:49 +0100

    Implemented some routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,59 @@
  * Still working out exactly how we want to implement this, prefably in a 
  * portable way.
  */ 
+import HelloWorld from './api/hello-world';
+const hello = new HelloWorld();
+
+import Personnel from './api/personnel';
+const personnel = new Personnel();
 
 const routes = generateRoutes();
 
 function generateRoutes(){
     //If function get post put delete@path
-    return [];
+    return [
+         
+        {
+            method: 'GET',
+            path: hello.path(),
+            handler: (request, reply) => {
+                reply( (hello.get(request): string) );
+            }
+        },
+
+        
+        {
+            method: 'GET',
+            path: personnel.path(),
+            handler: (request, reply) => {
+                reply( (personnel.read(request): Object) );
+            }
+        },
+
+        {
+            method: 'POST',
+            path: personnel.path(),
+            handler: (request, reply) => {
+                reply( (personnel.create(request): Object) );
+            }
+        },
+
+        {
+            method: 'UPDATE',
+            path: personnel.path()+'/{id}',
+            handler: (request, reply) => {
+                reply( (personnel.update(request): Object) );
+            }
+        },
+
+        {
+            method: 'DELETE',
+            path: personnel.path()+'/{id}',
+            handler: (request, reply) => {
+                reply( (personnel.delete(request): Object) );
+            }
+        }
+    ];
 }
 
 export default routes;
